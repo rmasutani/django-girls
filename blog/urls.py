fe6c589e7561edec02e961ea0cc18de5103b0369
rmasutani <masutanirugby@gmail.com>
2021-06-20 21:00:44 +0900

    Add post detail and post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    # <int:pk>
+    # 整数値を期待：値がpkという名前でviewに渡される
+    # 最後に '/' が必要
+    path("post/<int:pk>/", views.post_detail, name="post_detail")
 ]
